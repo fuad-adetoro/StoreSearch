54af153f5dbdf1bb689db1c8434f1d86dacae331
Frederico <BusyFredo@Fuads-MacBook-Pro.local>
2017-02-14 00:21:38 +0000

    Updated original view controller.

---
+++ StoreSearch/SearchViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Frederico on 13/02/2017.
+//  Copyright © 2017 Fuad Adetoro. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
