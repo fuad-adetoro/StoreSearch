d3267afc341b9b06a87cbc019e6d234d231c38c9
Frederico <BusyFredo@Fuads-MacBook-Pro.local>
2017-02-14 23:32:02 +0000

    Added a search bar and table view. The search puts fake results in the table for now.

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Frederico on 14/02/2017.
+//  Copyright © 2017 Fuad Adetoro. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -10,12 +10,99 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var searchResults: [SearchResult] = []
+    
+    var hasSearched = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
+        // Table view was conflicting with the search bar so I moved it down by 64 points.
+        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
     }
 
 
 }
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchResults = []
+        
+        if searchBar.text! != "Justin Bieber" {
+            for i in 0...2 {
+                let searchResult = SearchResult()
+                searchResult.name = String(format: "Fake result for %d'", i)
+                searchResult.artistName = searchBar.text!
+                searchResults.append(searchResult)
+            }
+        }
+        
+        hasSearched = true
+        tableView.reloadData()
+        searchBar.resignFirstResponder()
+    }
+    
+    func position(for bar: UIBarPositioning) -> UIBarPosition {
+        return .topAttached
+    }
+}
+
+extension SearchViewController: UITableViewDataSource {
+    
+    // Doesn't require override infront of function because UITableViewController isn't being subclassed and these methods haven't yet been defined.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searchResults.count == 0 {
+            if hasSearched {
+                return 1
+            }
+            
+            return 0
+        }
+        
+        return searchResults.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cellIdentifier = "SearchResultCell"
+        
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        
+        if cell == nil {
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
+        }
+        
+        if searchResults.count == 0 {
+            cell?.textLabel?.text = "(Nothing Found)"
+            cell?.detailTextLabel?.text = ""
+        } else {
+            let searchResult = searchResults[indexPath.row]
+            cell?.textLabel?.text = searchResult.name
+            cell?.detailTextLabel?.text = searchResult.artistName
+        }
+        
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        if searchResults.count == 0 {
+            return nil
+        } else {
+            return indexPath
+        }
+    }
+}
+
+extension SearchViewController: UITableViewDelegate {
+    
+}
+
 
