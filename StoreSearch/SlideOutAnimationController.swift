07c0ef9932092b628e1a0d7128a3e8f6c42297d4
Frederico <BusyFredo@Fuads-MacBook-Pro.local>
2017-02-17 03:56:55 +0000

    Animations added to the app.

---
+++ StoreSearch/SlideOutAnimationController.swift
@@ -0,0 +1,29 @@
+//
+//  SlideOutAnimationController.swift
+//  StoreSearch
+//
+//  Created by Frederico on 17/02/2017.
+//  Copyright © 2017 Fuad Adetoro. All rights reserved.
+//
+
+import UIKit
+
+class SlideOutAnimationController: NSObject, UIViewControllerAnimatedTransitioning {
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.3
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        if let fromView = transitionContext.view(forKey: UITransitionContextViewKey.from) {
+            let containerView = transitionContext.containerView
+            let duration = transitionDuration(using: transitionContext)
+            
+            UIView.animate(withDuration: duration, animations: {
+                fromView.center.y -= containerView.bounds.size.height
+                fromView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+            }, completion: { finished in
+                transitionContext.completeTransition(finished)
+            })
+        }
+    }
+}
